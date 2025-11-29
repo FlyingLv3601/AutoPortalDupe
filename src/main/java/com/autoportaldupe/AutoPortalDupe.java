28032c9e9c256992e95b13ac21ab5edb990e0fed
aleksandr Mihailov <aleksandrmihailov@aleksandrs-MacBook-Pro.local>
2025-11-29 04:48:53 +0200

    added test command

--- src/main/java/com/autoportaldupe/AutoPortalDupe.java
+++ src/main/java/com/autoportaldupe/AutoPortalDupe.java
@@ -1,14 +1,8 @@
 package com.autoportaldupe;
 
-
-
-
 import com.autoportaldupe.scanner.ChestPositionSave;
 import com.autoportaldupe.scanner.GetChestPos;
 import com.autoportaldupe.baritone.Goto;
-
-
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -18,16 +12,12 @@ import net.minecraft.text.Text;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 
-
-
-
 public class AutoPortalDupe implements ClientModInitializer {
 
     private boolean searching = false;
 
     @Override
     public void onInitializeClient() {
-
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(
                     ClientCommandManager.literal("Cpos")
@@ -46,54 +36,47 @@ public class AutoPortalDupe implements ClientModInitializer {
                             )
             );
 
-            dispatcher.register(ClientCommandManager.literal("TestChestPos")
+            dispatcher.register(
+                    ClientCommandManager.literal("TestChestPos")
                             .then(ClientCommandManager.argument("KitName", StringArgumentType.string())
                                     .executes(context -> {
                                         String kitName = StringArgumentType.getString(context, "KitName");
-
                                         double[] coordinates = GetChestPos.getKitCoordinates(kitName);
 
-                                        if (coordinates != null) {
-                                            double x = coordinates[0];
-                                            double y = coordinates[1];
-                                            double z = coordinates[2];
 
-                                            context.getSource().sendFeedback(Text.literal(
-                                                    String.format("Teleporting to kit '%s': %.1f %.1f %.1f", kitName, x, y, z)
-                                            ));
-
-                                            Goto.goToCoords(x, y, z);
-                                        } else {
-                                            context.getSource().sendFeedback(Text.literal("Kit not found: " + kitName));
-                                        }
+                                        double x = coordinates[0];
+                                        double y = coordinates[1];
+                                        double z = coordinates[2];
+                                        Goto.goToCoords(x, y, z);
 
                                         return 1;
                                     })
                             )
             );
 
+            dispatcher.register(
+                    ClientCommandManager.literal("pos")
+                            .then(ClientCommandManager.argument("standPos", StringArgumentType.string())
+                                    .executes(context -> {
+                                        String kitName = StringArgumentType.getString(context, "KitName");
+                                        MinecraftClient client = MinecraftClient.getInstance();
+                                        context.getSource().sendFeedback(Text.literal(String.format("Kit: %s is saved", kitName)));
+                                        double playerXpos = client.player.getX();
+                                        double playerZpos = client.player.getZ();
+                                        double playerYpos = client.player.getY();
 
-            dispatcher.register(ClientCommandManager.literal("pos")
-                    .then(ClientCommandManager.argument("standPos", StringArgumentType.string()))
+                                        ChestPositionSave.addPos(kitName, playerXpos, playerZpos, playerYpos);
+                                        return 1;
+                                    })
+                            )
+            );
+
+            dispatcher.register(
+                    ClientCommandManager.literal("dupeStart")
                             .executes(context -> {
-
-
-                                return 0;
+                                return 1;
                             })
             );
-
-
-            
-            dispatcher.register(ClientCommandManager.literal("dupeStart")
-                    .executes(context ->  {
-
-
-
-                        return 0;
-                    })
-            );
         });
-
-
     }
 }
